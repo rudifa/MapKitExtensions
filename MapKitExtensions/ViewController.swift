753a3ee4479a884fcc1cb0ae9c19211fd67e3e0d
Rudi Farkas <rudi.farkas@gmail.com>
2020-08-08 15:08:14 +0200

    Initial Commit

---
+++ MapKitExtensions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitExtensions
+//
+//  Created by Rudolf Farkas on 08.08.20.
+//  Copyright © 2020 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
